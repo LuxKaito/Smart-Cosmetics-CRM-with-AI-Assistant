e4bd0106fb734a24bbf7fb167c2d14a8338ff5a6
LuxKaito <khang.ng243@gmail.com>
2026-08-10 16:15:21 +0700

    chore(commitLint): custom configuration

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,1 +1,31 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
+    ],
+
+    'header-min-length': [2, 'always', 10],
+
+    'header-max-length': [2, 'always', 160],
+
+    'body-max-line-length': [2, 'always', 120],
+
+    'subject-case': [0],
+  },
+};
